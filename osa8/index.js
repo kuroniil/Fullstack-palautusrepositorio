5aa9445704b1dc8de7659110917877716ec43541
kuroniil <niilo.kuronen@helsinki.fi>
2025-12-25 14:02:59 +0200

    8.6

--- osa8/index.js
+++ osa8/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
+const { v1: uuid } = require("uuid");
 
 let authors = [
   {
@@ -111,6 +112,14 @@ const typeDefs = /* GraphQL */ `
     bookCount: Int!
     born: Int
   }
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int!
+      genres: [String]!
+    ): Book
+  }
 `;
 
 const resolvers = {
@@ -137,6 +146,20 @@ const resolvers = {
   Author: {
     bookCount: (root) => {
       return books.filter((book) => book.author === root.name).length;
+    },
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      const book = { ...args, id: uuid() };
+      books = books.concat(book);
+      if (!authors.find((a) => a.name === args.author)) {
+        authors = authors.concat({
+          name: args.author,
+          bookCount: 1,
+          id: uuid(),
+        });
+      }
+      return book;
     },
   },
 };
